967ec9db58adc2a38b2de892b926563a19c2638a
tricdt <tricdt@gmail.com>
2024-06-23 20:22:32 +0700

    TextBlock Inlines

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,17 @@ namespace WPF_Kteam
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            txbl3.Inlines.Add(new Run("www.HowKteam.com") { FontSize = 30 });
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start("cmd", "/c start " + e.Uri.ToString());
         }
     }
 }
